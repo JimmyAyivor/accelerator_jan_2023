43c33f94dc079eb5ad8173b78925a6ce2731e32c
Jimmy Ayivor <jimmyayivor@gmail.com>
2023-01-05 20:57:18 -0500

    add students controller & queries

--- 2023-01-04/studentAppStarter/app.js
+++ 2023-01-04/studentAppStarter/app.js
@@ -1,27 +1,14 @@
-/**
- * This file represents our Express API application
- *
- * TODOs:
- * - [] Create routes (using hardcoded JSON for now, not a real db):
- *  - [] Health check route to confirm the server is up and running
- *  - [] Get all students
- *  - [] Get student by ID
- *
- * - [] Organize our app into controller and query layers 
- * - [] Use a router to organize the routes by their prefixes (/students, /teachers, /posts, etc.)
- */
-
 const express = require('express');
-
-// Initialize app and general app configuration
 const app = express();
-app.use(express.json()); // Why do we need this?
 
-// Route handler functions
+const studentsController = require('./controllers/studentsController')
+
+app.use(express.json());
+
 app.get('/', (request, response) => {
-  console.log(request, response);
-
   response.json({ message: 'Service is running' });
 });
+
+app.use('/students', studentsController)
 
 module.exports = app;
